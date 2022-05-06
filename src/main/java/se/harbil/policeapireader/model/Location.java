7fbcfc90216d72a63f2aa0b4eba6441fea0141f6
Harald Billstein <harald.billstein@gmail.com>
2022-05-06 14:36:04 +0200

    fixed noArgConstructor

--- src/main/java/se/harbil/policeapireader/model/Location.java
+++ src/main/java/se/harbil/policeapireader/model/Location.java
@@ -1,11 +1,13 @@
 
 package se.harbil.policeapireader.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Location {
 
     private String gps;
