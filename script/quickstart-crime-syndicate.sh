18f70fc3b11520c88eafa4b04049ee682aae49ae
Harald Billstein <harald.billstein@gmail.com>
2022-05-06 16:08:29 +0200

    added docker-compose and script to run the app

---
+++ script/quickstart-crime-syndicate.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+bold=`tput bold`
+green=`tput setaf 2`
+blue=`tput setaf 4`
+reset=`tput sgr0`
+
+
+cd ..
+cd ..
+echo -e "${green}***********************************************************"
+echo -e "*${blue} ${bold}                 police-api-reader                      ${green}*"
+echo -e "***********************************************************${reset}"
+
+
+if [ -d "police-api-reader" ]
+then
+    echo "police-api-reader exists."
+else
+    echo "police-api-reader does not exists, cloning..."
+    git clone https://github.com/harald-billstein/police-api-reader.git
+fi
+
+cd police-api-reader
+git pull
+./mvnw clean install
+docker build -f ./Dockerfile -t police-api-reader:latest .
+
+
+#cd ..
+#echo -e "${green}***********************************************************"
+#echo -e "*${blue} ${bold}                 police-twitter                         ${green}*"
+#echo -e "***********************************************************${reset}"
+
+
+#if [ -d "police-twitter" ]
+#then
+#    echo "police-twitter exists."
+#else
+#    echo "police-twitter does not exists, cloning..."
+#fi
+
+
+#cd police-twitter
+#git pull
+#./mvnw clean install
+#docker build -f ./Dockerfile -t police-twitter:latest .
+
+docker-compose -p crime-syndicate -f ../police-api-reader/script/docker-compose.yml up -d
+
